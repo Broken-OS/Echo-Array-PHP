eb4fb9dd9df4931941ef9ac7ed67191f4980f39f
Broken-OS <73603426+Broken-OS@users.noreply.github.com>
2021-07-05 11:19:34 -0400

    Update function.php

--- function.php
+++ function.php
@@ -1,12 +1,16 @@
 <?php
 function EchoArray($array) {
-  $count = count($array);
-  $count = $count - 1;
-  foreach ($array as $echo) {
-    if($echo == $array[$count]) {
-      echo $echo, ".";
-    } else {
-      echo $echo, ", ";
+  if (empty($array)) {
+    echo "";
+  } else {
+    $count = count($array);
+    $count = $count - 1;
+    foreach ($array as $echo) {
+      if($echo == $array[$count]) {
+        echo $echo, ".";
+      } else {
+        echo $echo, ", ";
+      };
     };
   };
 };
